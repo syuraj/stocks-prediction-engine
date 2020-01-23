b8dc7826d60b08778026661dbe87798bf4e6b3ba
Suraj Shrestha <syuraj@gmail.com>
2020-01-23 00:42:12 -0500

    changing tensorflow version to fix heroku deploy issue

--- stock-playground.py
+++ stock-playground.py
@@ -1,12 +1,12 @@
 
 # %%
-from lstm.LSTM_model import build_model_and_predict
-from datetime import date
-from prophet_predictor import ProphetPredictor
-from stocker import Stocker
-from environs import Env
 import sys
 sys.path.insert(0, "../")
+from lstm.LSTM_model import build_model_and_predict
+from datetime import date
+from prophet.prophet_predictor import ProphetPredictor
+from prophet.stocker import Stocker
+from environs import Env
 
 env = Env()
 api_key = env('alphavantage_api_key')
